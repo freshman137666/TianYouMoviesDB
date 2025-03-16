224180300b1ab9c19aea86e9e19c7323717b2573
freshman137666 <140179221+freshman137666@users.noreply.github.com>
2025-03-16 10:10:03 +0800

    项目基础框架

---
+++ TVMoviesDB/src/main/java/com/example/mysqltext/MysqlTextApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqltext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlTextApplication.class, args);
+    }
+
+}
